f34b0cbbc3a4dfd610a8dfcd61e6fff29d07150d
TomasDolejsek <tdolejsek@centrum.cz>
2024-03-04 16:10:32 +0100

    level2

---
+++ 01 Introduction to JavaScript/01 My First Project/my_first_project.js
@@ -0,0 +1,18 @@
+const input = require('sync-input');
+
+let total = 0.0;
+console.log("Earned amount:");
+console.log("Bubblegum: $202");
+total += 202;
+console.log("Toffee: $118");
+total += 118;
+console.log("Ice cream: $2250");
+total += 2250;
+console.log("Milk chocolate: $1680");
+total += 1680;
+console.log("Doughnut: $1075");
+total += 1075;
+console.log("Pancake: $80");
+total += 80;
+console.log("\nIncome: $" + total);
+
